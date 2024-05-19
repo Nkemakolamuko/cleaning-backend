aee8d2c8d2221c9236b82d71c4bf42afdf7c0519
Nkemakolauko <nwekevino99@gmail.com>
2024-05-19 22:51:04 +0100

    Quick fix --

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const productRoute = require("./routes/product.route.js");
-const userRoute = require("./routes/user.route.js");
-const cleanersRoute = require("./routes/cleaner.route.js");
+// const productRoute = require("./routes/product.route.js");
+// const userRoute = require("./routes/user.route.js");
+// const cleanersRoute = require("./routes/cleaner.route.js");
 const dbConnect = require("./db/dbConnect.js");
-const errorHandler = require("./middleware/errorHandler.js");
+// const errorHandler = require("./middleware/errorHandler.js");
 
 const app = express();
 // CORS
@@ -18,19 +18,19 @@ const app = express();
 // );
 app.use(cors());
 app.options("*", cors());
-dbConnect();
-const PORT = process.env.PORT || 5001;
+// dbConnect();
+// const PORT = process.env.PORT || 5001;
 
-// Middlewares
-// Normal JSON
-app.use(express.json());
-app.use(errorHandler);
-// This one allows for getting values from form
-app.use(express.urlencoded({ extended: false }));
+// // Middlewares
+// // Normal JSON
+// app.use(express.json());
+// app.use(errorHandler);
+// // This one allows for getting values from form
+// app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/products", productRoute); // The user will make request to this route - so it's post - it's a base url, the methods are in the routes folder
-app.use("/api/users", userRoute); // For registering users
-app.use("/api/cleaners", cleanersRoute); // For getting cleaners nearby
+// app.use("/api/products", productRoute); // The user will make request to this route - so it's post - it's a base url, the methods are in the routes folder
+// app.use("/api/users", userRoute); // For registering users
+// app.use("/api/cleaners", cleanersRoute); // For getting cleaners nearby
 
 app.get("/", (req, res) => {
   res.send("Holla... connected and working.");
