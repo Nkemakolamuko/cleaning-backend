35cd812bf1dc9b40a39bcd2fece48ea6c85d600b
Nkemakolauko <nwekevino99@gmail.com>
2024-05-19 21:40:46 +0100

    Refactor and fix error --

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const productRoute = require("./routes/product.route.js");
 const userRoute = require("./routes/user.route.js");
 const cleanersRoute = require("./routes/cleaner.route.js");
 const dbConnect = require("./db/dbConnect.js");
 const errorHandler = require("./middleware/errorHandler.js");
-require("dotenv").config();
 
-// CORS configuration
-app.use(
-  cors({
-    origin: "https://247-cleaning.vercel.app",
-    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
-  })
-);
+const app = express();
+// CORS
+// app.use(
+//   cors({
+// origin: "https://247-cleaning.vercel.app",
+//     origin: "https://247-cleaning.vercel.app",
+//     methods: ["POST, GET, PUT, PATCH, DELETE"],
+//   })
+// );
+app.use(cors());
 app.options("*", cors());
-
+dbConnect();
 const PORT = process.env.PORT || 5001;
 
-// Connect to database
-dbConnect();
-
 // Middlewares
+// Normal JSON
 app.use(express.json());
+app.use(errorHandler);
+// This one allows for getting values from form
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
-app.use("/api/products", productRoute);
-app.use("/api/users", userRoute);
-app.use("/api/cleaners", cleanersRoute);
+app.use("/api/products", productRoute); // The user will make request to this route - so it's post - it's a base url, the methods are in the routes folder
+app.use("/api/users", userRoute); // For registering users
+app.use("/api/cleaners", cleanersRoute); // For getting cleaners nearby
 
-// Root route
 app.get("/", (req, res) => {
   res.send("Holla... connected and working.");
 });
 
-// Error handling middleware
-app.use(errorHandler);
-
-// Start server
 const start = () => {
+  dbConnect();
   app.listen(PORT, () => console.log(`Running on port ${PORT}`));
 };
 
